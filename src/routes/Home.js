041ff19a405d2493f3418adc3dcdf96160b12eb2
Sophie <jihye340@gmail.com>
2021-11-03 22:45:33 +0900

    6.1 Styles

---
+++ src/routes/Home.js
@@ -0,0 +1,36 @@
+import Nweet from "components/Nweet";
+import { dbService } from "fbase";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import { useState, useEffect, useRef } from "react";
+import NweetFactory from "components/NweetFactory";
+
+const Home = ({ userObj }) => {
+    const [nweets, setNweets] = useState([]);
+    const fileInput = useRef();
+    useEffect(() => {
+        const q = query(
+            collection(dbService, "nweets"),
+            orderBy("createdAt", "desc")
+        );
+        onSnapshot(q, (snapshot) => {
+            console.log(q, snapshot);
+            const nweetArr = snapshot.docs.map((doc) => ({
+                id:doc.id,
+                ...doc.data(),
+            }));
+            setNweets(nweetArr);
+        });
+    }, []);
+    
+    return (
+        <div className="container">
+            <NweetFactory userObj={userObj}/>
+            <div style={{ marginTop: 30 }}>
+                {nweets.map((nweet) => (
+                    <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
+                ))}
+            </div>
+        </div>
+    );
+};
+export default Home;
